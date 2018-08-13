0d5425f3b9ce69a4078a5bdc4f3916ff3bb3ddf9
Alolika  Dutta <alolikadutta@Alolikas-MacBook-Air.local>
2018-08-12 20:41:04 -0400

    initial save 8/12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+
+import { ChatpageComponent } from './chatpage/chatpage.component';
+import { ChatmessageComponent } from './chatpage/chatmessage/chatmessage.component';
+import { SidebarComponent } from './chatpage/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    SidebarComponent,
+    ChatpageComponent,
+    ChatmessageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/chatpage/chatmessage/chatmessage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chatmessage',
+  templateUrl: './chatmessage.component.html',
+  styleUrls: ['./chatmessage.component.css']
+})
+export class ChatmessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
